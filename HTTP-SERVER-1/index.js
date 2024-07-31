81bea8bd14ffc450c86ead7ce6115d2a9bd8edd7
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-01 03:32:54 +0530

    post requests handling

--- HTTP-SERVER-1/index.js
+++ HTTP-SERVER-1/index.js
@@ -4,12 +4,25 @@ const bodyParser = require("body-parser");
 const port = 3000
 const app = express();
 
-// middlewares
-app.use(bodyParser.json());
-app.post('/', function(req, res) {
-    console.log(req.body)
+app.get('/', function(req, res) {
     res.send('Hello World!')
 })
+
+// middlewares
+app.use(bodyParser.json());
+
+// app.post('/', function(req, res) {
+//     console.log(req.body)
+//     res.send('Hello World!')
+// })
+
+// app.post('/backend-api/conversation', function(req, res) {
+//     const message = req.query.message;
+//     console.log(message)
+//     res.json({
+//         output: "2 + 2 = 4"
+//     })
+// })
 
 // app.get("/zaid-handler", function (req, res) {
 //     res.json({
