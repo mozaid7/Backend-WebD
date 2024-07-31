2996b6fef3bda21187336f4855a2a75e53b0c027
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-01 01:33:07 +0530

    simple backend code in express

--- HTTP-SERVER-1/index.js
+++ HTTP-SERVER-1/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port)
