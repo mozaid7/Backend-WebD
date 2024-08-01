55605755a0a62ef2c6e66ecc9533504cc6da2d7c
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-01 20:42:25 +0530

    spacing and presentation edited

--- HTTP-SERVER-1/index.js
+++ HTTP-SERVER-1/index.js
@@ -6,11 +6,46 @@ const express = require('express');
 const port = 3000
 const app = express();
 
-// app.get('/', function(req, res) {
+app.get('/', function(req, res) {
+    res.send('Hello World!')
+})
+
+// app.post('/', function(req, res) {
+//     console.log(req.body)
 //     res.send('Hello World!')
+// }) 
+
+// middlewares
+//app.use(bodyParser.json());
+// app.post('/backend-api/conversation', function(req, res) {
+//     const message = req.query.message;
+//     console.log(message)
+//     res.json({
+//         output: "2 + 2 = 4"
+//     })
 // })
 
+// app.get("/zaid-handler", function (req, res) {
+//     res.json({
+//         name: "Zaid",
+//         age: 20,
+//    });
+//  });
+
+
 // Logic Creation on HTTP server created 
+// middlewares
+// app.use(bodyParser.json());
+// app.post('/backend-api/conversation', function(req, res) {
+//     const message = req.query.message;
+//     console.log(message)
+//     res.json({
+//         output: "2 + 2 = 4"
+//     })
+// })
+
+
+//used for adding input to the function that we are calling syntax => localhost:{portnumber}/?n=value
 // For providing a parameter during a server request, use ?n=3   or ?n=4, etc.
 function sum(n) {
     let ans = 0;
@@ -24,27 +59,5 @@ app.get("/", function(req, res) {
     const ans = sum(n);
     res.send("Hi youri ans s " + ans);
 })
-
-// app.post('/', function(req, res) {
-//     console.log(req.body)
-//     res.send('Hello World!')
-// }) 
-
-// app.get("/zaid-handler", function (req, res) {
-//     res.json({
-//         name: "Zaid",
-//         age: 20,
-//    });
-//  });
-
-// middlewares
-//app.use(bodyParser.json());
-// app.post('/backend-api/conversation', function(req, res) {
-//     const message = req.query.message;
-//     console.log(message)
-//     res.json({
-//         output: "2 + 2 = 4"
-//     })
-// })
 
 app.listen(port)
