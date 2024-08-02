b7d2f66fd2fc6e6c81ea53f33d21440daca8f2f1
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-03 02:17:36 +0530

    demo data added to database

--- HTTP-SERVER-1/mongoose.js
+++ HTTP-SERVER-1/mongoose.js
@@ -9,25 +9,25 @@ mongoose.connect(
 );
 
 
-app.post("/signup", async function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  const name = req.body.name;
+// app.post("/signup", async function (req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   const name = req.body.name;
 
-  const existingUser = await User.findOne({ email: username });
-  if (existingUser) {
-    return res.status(400).send("Username already exists!!!");
-  }
-  else{
-    return res.send("No user found sss")
-  }
-});
+//   const existingUser = await User.findOne({ email: username });
+//   if (existingUser) {
+//     return res.status(400).send("Username already exists!!!");
+//   }
+//   else{
+//     return res.send("No user found sss")
+//   }
+// });
 
-const User = mongoose.model("Users", {
-  name: String,
-  email: String,
-  password: String,
-});
+// const User = mongoose.model("Users", {
+//   name: String,
+//   email: String,
+//   password: String,
+// });
 
 const user = new User({ name: "b", email: "bcd@123", password: "bcd" });
 user.save();
