1e490d04ddfdecf54d6f4313760e079baf597a5b
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-02 15:05:28 +0530

    adding logic for middlewares

---
+++ HTTP-SERVER-1/index3.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const port = 3000
+const app = express();
+app.use(express.json());
+
+app.get("/health-checkup", function(req, res) {
+   const kidneyId = req.query.kidneyId;
+   const username = req.headers.username;
+   const password = req.headers.password;
+
+  if (username != "harkirat" || password != "pass") {
+     res.status(400).json({"msg": "something up with your inputs"})
+     return
+    } 
+   if (kidneyId != 1 || kidneyId != 2) {
+     res.status(400).json({"msg": "something up with your inputs"})
+     return
+    }
+   // do something with kidney
+   res.json({
+    msg: "your kidney is fine!"
+   })
+});
