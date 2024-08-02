025d97cbe6f9b94049b6693a00ff9d765850a5ff
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-02 23:58:42 +0530

    comments regarding zod

--- HTTP-SERVER-1/index5.js
+++ HTTP-SERVER-1/index5.js
@@ -1,8 +1,8 @@
+// ZOD let's you check or validate whether the provided input is valid or not.
+
 const express = require("express");
 const zod = require("zod");
 const app = express();
-
-const schema = zod.array(zod.number());
 
 // How to write zod conditions for different schema
 // {
@@ -17,20 +17,44 @@ const schema = zod.array(zod.number());
 //     kidneys: z.array(z.number())
 // })
 
-app.use(express.json());
-
-app.post("/health-checkup", (req, res) => {
-  const kidneys = req.body.kidneys;
-  const response = schema.safeParse(kidneys)
-  if (!response.success) {
-    res.status(411).json({
-        msg: "input is invalid"
+function validateInput(obj) {
+    const schema = zod.object ({
+        email: zod.string().email(),
+        password: zod.string().min(8)
     })
-  } else {
-    res.send({
-        response
-      })
+    const response = schema.safeParse(obj);
+    console.log(response);
+}
+validateInput ({
+    email: "hkirat@gmail.com",
+    password: "1235456789"
+});
+
+app.use(express.json());
+app.post("/login", (req, res) => {
+  const response = validateInput(req.body)
+  if (!response.success) {
+    res.json({
+        msg: "Input is invalid"
+    })
+    return
   }
 });
+
+// const schema = zod.array(zod.number());
+// app.use(express.json());
+// app.post("/health-checkup", (req, res) => {
+//   const kidneys = req.body.kidneys;
+//   const response = schema.safeParse(kidneys)
+//   if (!response.success) {
+//     res.status(411).json({
+//         msg: "input is invalid"
+//     })
+//   } else {
+//     res.send({
+//         response
+//       })
+//   }
+// });
   
   app.listen(3000);
