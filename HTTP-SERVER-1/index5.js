f43198063021d750c2afa2518efc98cbf42b3152
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-02 21:09:17 +0530

    zod writing

--- HTTP-SERVER-1/index5.js
+++ HTTP-SERVER-1/index5.js
@@ -4,14 +4,33 @@ const app = express();
 
 const schema = zod.array(zod.number());
 
+// How to write zod conditions for different schema
+// {
+//     email: string
+//     password: atleast 8 letters
+//     country: "IN", "US"
+// }
+// const schema = zod.object({
+//     email: zod.string(),
+//     password: z.string(),
+//     country: z.literal("IN").or(z.literal("US")),
+//     kidneys: z.array(z.number())
+// })
+
 app.use(express.json());
 
 app.post("/health-checkup", (req, res) => {
   const kidneys = req.body.kidneys;
   const response = schema.safeParse(kidneys)
-  res.send({
-    response
-  })
+  if (!response.success) {
+    res.status(411).json({
+        msg: "input is invalid"
+    })
+  } else {
+    res.send({
+        response
+      })
+  }
 });
   
   app.listen(3000);
