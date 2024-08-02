667ae95c235eda94608c685594ab1eb321b5f7f3
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-03 02:50:21 +0530

    bug fixes in MongoDB code

--- HTTP-SERVER-1/mongoose.js
+++ HTTP-SERVER-1/mongoose.js
@@ -8,27 +8,34 @@ mongoose.connect(
   "mongodb+srv://mozaid7:mohammedzaid98@cluster7.v0zmtq7.mongodb.net/"
 );
 
-
-// app.post("/signup", async function (req, res) {
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   const name = req.body.name;
-
-//   const existingUser = await User.findOne({ email: username });
-//   if (existingUser) {
-//     return res.status(400).send("Username already exists!!!");
-//   }
-//   else{
-//     return res.send("No user found sss")
-//   }
-// });
-
 const User = mongoose.model("Users", {
-  name: String,
-  email: String,
-  password: String,
-});
+    name: String,
+    email: String,
+    password: String,
+  });
 
-const user = new User({ name: "b", email: "bcd@123", password: "bcd" });
-user.save();
-app.listen(3000);
+app.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  const existingUser = await User.findOne({ email: username });
+  if (existingUser) {
+    return res.status(400).send("Username already exists!!!");
+  }
+  const user = new User ({ 
+    name: name, 
+    email: username, 
+    password: password 
+  });
+  
+ user.save();
+ res.json ({
+     "msg": "User created successfully"
+ })
+
+ app.listen(3000);
+
+}); 
+
+
