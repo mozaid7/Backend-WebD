00c5fed6ab9587df4055a05945ccadfe8791552a
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-23 21:15:53 +0530

    .env file variable exporting prac.

--- tsprac/src/index.ts
+++ tsprac/src/index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { Client } from 'pg';
 
-const key: string = process.env.Password || '';
+const key = process.env.Password;
 
 export async function getClient() {
     const client = new Client(`postgres://avnadmin:${key}@pg-2cbdf470-mohammedzee98-2970.h.aivencloud.com:17452/defaultdb?sslmode=require`);
